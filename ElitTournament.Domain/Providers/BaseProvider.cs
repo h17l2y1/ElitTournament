b8bb5b3f2417c6ebf2f54846b42f9c72990c835d
vlad <vlad.h17l2y1@gamil.com>
2020-01-10 10:24:08 +0200

    temp

---
+++ ElitTournament.Domain/Providers/BaseProvider.cs
@@ -0,0 +1,36 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using ElitTournament.Domain.Helpers.Interfaces;
+using ElitTournament.Domain.Providers.Interfaces;
+using System.Threading.Tasks;
+
+namespace ElitTournament.Domain.Providers
+{
+	public class BaseProvider : IBaseProvider
+	{
+		protected readonly IHtmlLoaderHelper _htmlLoaderHelper;
+		protected readonly IConfiguration _сonfiguration;
+
+		protected readonly HtmlParser domParser;
+
+		public BaseProvider(IHtmlLoaderHelper htmlLoaderHelper, IConfiguration сonfiguration)
+		{
+			_htmlLoaderHelper = htmlLoaderHelper;
+			_сonfiguration = сonfiguration;
+			domParser = new HtmlParser();
+		}
+
+		public string ScheduleUrl { get; set; }
+
+		public string TableUrl { get; set; }
+
+		public async Task<IDocument> GetPage(string url)
+		{
+			IHtmlDocument source = await _htmlLoaderHelper.GetPageSource(url);
+			IDocument document = await domParser.ParseDocumentAsync(source);
+			return document;
+		}
+	}
+}
