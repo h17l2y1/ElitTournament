a0048f347e8ca213746635e8c3ab04d68819b1a5
vlad <vlad.h17l2y1@gamil.com>
2020-01-15 10:23:29 +0200

    add(TelegramBot): add buttons

--- ElitTournament.Domain/Commands/StartCommand.cs
+++ ElitTournament.Domain/Commands/StartCommand.cs
@@ -1,5 +1,8 @@
-﻿using ElitTournament.Domain.Services.Interfaces;
+﻿using ElitTournament.Domain.Entities;
+using ElitTournament.Domain.Services.Interfaces;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -10,11 +13,13 @@ namespace ElitTournament.Domain.Commands
     public class StartCommand : Command
     {
 		private readonly IScheduleService _scheduleService;
+		private readonly List<League> Leagues;
 
 		public StartCommand(IScheduleService scheduleService) 
 			: base("расписание")
         {
 			_scheduleService = scheduleService;
+			Leagues = new List<League>();
 			Text = @"Для просмотра расписание выберите лигу, а потом команду.";
         }
 
@@ -29,34 +34,55 @@ namespace ElitTournament.Domain.Commands
         private ReplyKeyboardMarkup GetMenu()
         {
             var menu = new ReplyKeyboardMarkup();
-            menu.Keyboard = new[]
-            {
-                new[]{
-                new KeyboardButton("Elit-лига"),
-                new KeyboardButton("1-лига"),               
-                },
-                new[]{
-                    new KeyboardButton("2-лига группа А"),
-                    new KeyboardButton("2-лига группа Б"),
-                },
-                new[]{
-                    new KeyboardButton("3-лига"),
-                    new KeyboardButton("4-лига"),
-                },
 
-                new[]{
-                    new KeyboardButton("5-лига"),
-                    new KeyboardButton("6-лига"),
-                },
-                new[]{
-                    new KeyboardButton("7-лига"),
-                    new KeyboardButton("8-лига"),
-                },
-                new[]{
-                    new KeyboardButton("9-лига"),
-                    new KeyboardButton("10-лига"),
-                },
-            };
+
+			//List<IEnumerable<KeyboardButton>> list = new List<IEnumerable<KeyboardButton>>();
+			List<List<KeyboardButton>> list = new List<List<KeyboardButton>>();
+
+			for (int i = 0; i < Leagues.Count; i++)
+			{
+				List<KeyboardButton> test = new List<KeyboardButton>
+				{
+					new KeyboardButton(Leagues[i].Name),
+					new KeyboardButton(Leagues[i + 1].Name)
+				};
+
+				//IEnumerable<KeyboardButton> enu = test.AsEnumerable();
+
+				list.Add(test);
+			}
+			//IEnumerable<IEnumerable<KeyboardButton>> enu1 = list.AsEnumerable();
+
+			menu.Keyboard = list;
+
+			//menu.Keyboard = new[]
+   //         {
+   //             new[]{
+   //             new KeyboardButton("Elit-лига"),
+   //             new KeyboardButton("1-лига"),               
+   //             },
+   //             new[]{
+   //                 new KeyboardButton("2-лига группа А"),
+   //                 new KeyboardButton("2-лига группа Б"),
+   //             },
+   //             new[]{
+   //                 new KeyboardButton("3-лига"),
+   //                 new KeyboardButton("4-лига"),
+   //             },
+
+   //             new[]{
+   //                 new KeyboardButton("5-лига"),
+   //                 new KeyboardButton("6-лига"),
+   //             },
+   //             new[]{
+   //                 new KeyboardButton("7-лига"),
+   //                 new KeyboardButton("8-лига"),
+   //             },
+   //             new[]{
+   //                 new KeyboardButton("9-лига"),
+   //                 new KeyboardButton("10-лига"),
+   //             },
+   //         };
 
             return menu;
         }
