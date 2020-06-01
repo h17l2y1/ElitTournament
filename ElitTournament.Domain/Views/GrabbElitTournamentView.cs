2332a8c63517c53c8f8867e07cea1420838470d4
vlad <vladislavvillya@gmail.com>
2020-06-01 20:55:23 +0300

    new view

---
+++ ElitTournament.Domain/Views/GrabbElitTournamentView.cs
@@ -0,0 +1,17 @@
+﻿using ElitTournament.Domain.Entities;
+using System.Collections.Generic;
+
+namespace ElitTournament.Domain.Views
+{
+	public class GrabbElitTournamentView
+	{
+		public List<Schedule> Schedule { get; set; }
+		public List<League> Leagues { get; set; }
+
+		public GrabbElitTournamentView(List<Schedule> schedule, List<League> leagues)
+		{
+			Schedule = schedule;
+			Leagues = leagues;
+		}
+	}
+}
